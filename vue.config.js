5c48bbac58495821080968aa97042a6433e1f1a1
tkgstrator <nasawake.am@gmail.com>
2020-09-17 12:35:58 -0400

    Update Router Config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  publicPath: process.env.NODE_ENV === "production"
+    ? "/VueJSTemplate/"
+    : "/",
+  productionSourceMap: false,
+  assetsDir: "",
+  outputDir: "./dist"
+
 }
